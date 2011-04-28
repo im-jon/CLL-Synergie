72c539fee53e4cc3e3e05f0b777a555dfd40b82e
Jonathan Pouliot <jonathan.pouliot@gmail.com>
2011-04-28 18:56:46 -0400

    Enlevé Nettoyeur.sh, prise 2

--- Nettoyeur.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm Client/Client_Synergie/Client_Synergie.pro.user
-rm Client/QScintilla/Qt4/qscintilla.pro.user
-rm Serveur/Serveur_Synergie/Serveur_Synergie.pro.user
-rm Synergie.pro.user
