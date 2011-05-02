64eced6f95177e71b388bc0722fa950b65d5898d
Jonathan Pouliot <pouliot.jonathan@gmail.com>
2011-05-02 08:24:26 -0400

    Ajout et intégration du paquet serveur #2

---
+++ Serveur/Serveur_Synergie/Reseau/Paquets/paquetlistefichiers.h
@@ -0,0 +1,12 @@
+#ifndef PAQUETLISTEFICHIERS_H
+#define PAQUETLISTEFICHIERS_H
+
+#include "basepaquetserveur.h"
+
+class PaquetListeFichiers : public BasePaquetServeur
+{
+public:
+    PaquetListeFichiers();
+};
+
+#endif // PAQUETLISTEFICHIERS_H
